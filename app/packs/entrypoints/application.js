171e698fcfaafbdb7ac4619ef762cad0e359c0bf
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2022-02-16 01:06:57 -0300

    replace to turbo

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -13,7 +13,6 @@ import "core-js/stable"
 import "regenerator-runtime/runtime"
 import * as ActiveStorage from "@rails/activestorage"
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 
 Rails.start()
 ActiveStorage.start()
